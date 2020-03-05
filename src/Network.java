9cc7b92720842d168a84024104bec32cb585bf40
s1920176 <z.liu-63@sms.ed.ac.uk>
2020-03-05 01:29:46 +0000

    finish part A

---
+++ src/Network.java
@@ -0,0 +1,312 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+Class to simulate the network. System design directions:
+
+- Synchronous communication: each round lasts for 20ms
+- At each round the network receives the messages that the nodes want to send and delivers them
+- The network should make sure that:
+	- A node can only send messages to its neighbours
+	- A node can only send one message per neighbour per round
+- When a node fails, the network must inform all the node's neighbours about the failure
+*/
+
+public class Network {
+
+    private static List<Node> nodes;
+    private int round;
+    private int period = 20;
+    private Map<Integer, String> msgToDeliver; //Integer for the id of the sender and String for the message
+    private List<Integer> elect_round;
+    private Map<Integer, ArrayList<String>> elect_nodes;
+    private Map<String, Node> savedNode;
+    public final String lock = "";
+
+    public void NetSimulator() throws IOException, InterruptedException {
+        //list to store all nodes
+        nodes = new ArrayList<Node>();
+        //map to store what to deliver each round
+        msgToDeliver = new HashMap<Integer, String>();
+        //map to match the id to the node
+        savedNode = new HashMap<String, Node>();
+        //list to store all elect rounds
+        elect_round = new ArrayList<Integer>();
+        //map to store which nodes start to elect on specific round
+        elect_nodes = new HashMap<Integer, ArrayList<String>>();
+
+
+        round = 1;
+
+        this.parseFile("input/ds_graph.txt");
+        this.parseElectFile("input/ds_elect.txt");
+
+        for (Node node : nodes) {
+            node.setNetwork(this);
+        }
+
+        startElection();
+    }
+
+    public void startElection() throws InterruptedException {
+        long start_time = System.currentTimeMillis();
+        boolean thing_todo = false;
+        int leader_node = -1;
+
+        /*
+        Code to call methods for parsing the input file, initiating the system and producing the log can be added here.
+        */
+        System.out.println("This is round"+round+"\n");
+        while (round<30)
+        {
+            thing_todo = false;
+            leader_node = -1;
+            start_time = System.currentTimeMillis();
+            while ((System.currentTimeMillis() - start_time) < period)
+            {
+                if(!thing_todo &&elect_round.contains(round))
+                {
+                    System.out.println("This is elect round"+round);
+                    for (int i=0; i<elect_nodes.get(round).size();i++)
+                    {
+                        savedNode.get(elect_nodes.get(round).get(i)).setParticipant(true);
+                        savedNode.get(elect_nodes.get(round).get(i)).setElection(true);
+                    }
+                }
+                if(!thing_todo &&round==1)
+                {
+                    for (Node node : nodes)
+                    {
+                        node.start();
+                    }
+                }
+                if(!thing_todo && round>1)
+                {
+                    deliverMessages();
+                    synchronized (Network.class)
+                    {
+                        Network.class.notifyAll();
+                    }
+                }
+                thing_todo=true;
+            }
+            round++;
+        }
+    }
+
+    public int getRound()
+    {
+        return this.round;
+    }
+
+    private void parseFile(String filename) throws IOException {
+   		/*
+   		Code to parse the file can be added here. Notice that the method's descriptor must be defined.
+   		*/
+
+        FileInputStream inputStream = new FileInputStream(filename);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String str = null;
+
+        String previous_node = null;
+
+        Node start_node = null;
+
+        while((str = bufferedReader.readLine()) != null)
+        {
+
+            String [] node_in_line = str.split("\\s+");
+
+            Node head_node;
+
+            //add or get head node
+            if(!savedNode.containsKey(node_in_line[0]))
+            {
+                head_node = new Node(Integer.parseInt(node_in_line[0]));
+                savedNode.put(node_in_line[0],head_node);
+                nodes.add(head_node);
+            }
+            else
+            {
+                head_node = savedNode.get(node_in_line[0]);
+            }
+
+            if(start_node==null)
+                start_node = head_node;
+
+            if(previous_node!=null)
+            {
+                savedNode.get(previous_node).setNextNode(head_node);
+                if(!savedNode.get(previous_node).getNeighbors().contains(head_node))
+                    savedNode.get(previous_node).addNeighbour(head_node);
+                if(!head_node.getNeighbors().contains(savedNode.get(previous_node)))
+                    head_node.addNeighbour(savedNode.get(previous_node));
+                System.out.println("node "+previous_node+" and "+node_in_line[0]+" becomes neighbours");
+            }
+            previous_node = node_in_line[0];
+
+            //add neighbours
+            Node nei_node;
+            for(int i=1;i<node_in_line.length;i++)
+            {
+                if(!savedNode.containsKey(node_in_line[i]))
+                {
+                    System.out.println(node_in_line[i]+" is not in hash table");
+                    nei_node = new Node(Integer.parseInt(node_in_line[i]));
+                    savedNode.put(node_in_line[i],nei_node);
+                    nodes.add(nei_node);
+
+                }
+                else
+                {
+                    System.out.println(node_in_line[i]+" is already in hash table");
+                    nei_node = savedNode.get(node_in_line[i]);
+                }
+                System.out.println("node "+node_in_line[0]+" add neighbor node "+node_in_line[i]);
+                head_node.addNeighbour(nei_node);
+            }
+
+        }
+
+        //link the tail and the head of the ring
+        if(start_node!=null)
+        {
+            savedNode.get(previous_node).setNextNode(start_node);
+            start_node.addNeighbour(savedNode.get(previous_node));
+            savedNode.get(previous_node).addNeighbour(start_node);
+            System.out.println("node "+previous_node+" and "+start_node+" becomes neighbours");
+        }
+
+        //close
+        inputStream.close();
+        bufferedReader.close();
+    }
+
+    private void parseElectFile(String filename) throws IOException {
+   		/*
+   		Code to parse the file can be added here. Notice that the method's descriptor must be defined.
+   		*/
+
+        FileInputStream inputStream = new FileInputStream(filename);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String str;
+
+        String elect_parse = "ELECT";
+
+        String fail_parse = "FAIL";
+
+
+        while((str = bufferedReader.readLine()) != null) {
+
+            String[] element_in_line = str.split("\\s+");
+
+            if(element_in_line[0].equals(elect_parse))
+            {
+                elect_round.add(Integer.parseInt(element_in_line[1]));
+
+                ArrayList<String> temp = new ArrayList<String>();
+
+                for (int i = 2; i < element_in_line.length; i++) {
+
+                    System.out.println(element_in_line[i] + " is going to start elect in round " + element_in_line[1]);
+
+                    temp.add(element_in_line[i]);
+                }
+                elect_nodes.put(Integer.parseInt(element_in_line[1]), temp);
+            }
+
+            if(element_in_line[0].equals(fail_parse))
+            {
+                elect_round.add(Integer.parseInt(element_in_line[1]));
+
+                ArrayList<String> temp = new ArrayList<String>();
+
+                for (int i = 2; i < element_in_line.length; i++) {
+
+                    System.out.println(element_in_line[i] + " is going to start elect in round " + element_in_line[1]);
+
+                    temp.add(element_in_line[i]);
+                }
+                elect_nodes.put(Integer.parseInt(element_in_line[1]), temp);
+            }
+
+        }
+
+        //close
+        inputStream.close();
+        bufferedReader.close();
+    }
+
+
+    public synchronized void addMessage(int id, String m){
+		/*
+		At each round, the network collects all the messages that the nodes want to send to their neighbours.
+		Implement this logic here.
+		*/
+        this.msgToDeliver.put(id, m);
+    }
+
+    public synchronized void deliverMessages() {
+		/*
+		At each round, the network delivers all the messages that it has collected from the nodes.
+		Implement this logic here.
+		The network must ensure that a node can send only to its neighbours, one message per round per neighbour.
+		0 means no message to send or the sender will send the message to all his neighbours
+		*/
+
+
+            try {
+                for (Integer key : msgToDeliver.keySet()) {
+                    if(msgToDeliver.get(key).equals(Character.toString('0')))
+                    {
+                        continue;
+                    }
+                    savedNode.get(Integer.toString(key)).getNextNode().receiveMsg(msgToDeliver.get(key));
+                    msgToDeliver.put(key, Character.toString('0'));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+    }
+
+    public synchronized void informNodeFailure(int id) {
+		/*
+		Method to inform the neighbours of a failed node about the event.
+		*/
+        Node failed_node = savedNode.get(Integer.toString(id));
+        for (Node node : failed_node.getNeighbors())
+        {
+            node.myNeighbours.remove(failed_node);
+            if(node.myNeighbours.isEmpty())
+            {
+                System.out.println("network error");
+            }
+            if(node.next_neighbour==failed_node)
+            {
+                node.next_neighbour = node.myNeighbours.get(0);
+            }
+            if(node.prev_neighbour==failed_node)
+            {
+                node.prev_neighbour = node.myNeighbours.get(0);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+		/*
+		Your main must get the input file as input.
+		*/
+        Network netWork = new Network();
+        netWork.NetSimulator();
+    }
+
+}
+
---
+++ src/Node.java
@@ -0,0 +1,220 @@
+import java.util.*;
+import java.io.*;
+
+/* Class to represent a node. Each node must run on its own thread.*/
+
+public class Node extends Thread {
+
+    private int id;
+    private boolean participant = false;
+    private boolean leader = false;
+    private int leader_id = 0;
+    private boolean start_election = false;
+    private int fail_round = 0;
+    private Network network;
+
+    // Neighbouring nodes
+    public List<Node> myNeighbours;
+
+    // Queues for the incoming messages
+    public List<String> incomingMsg;
+
+    public Node next_neighbour;
+
+    public Node prev_neighbour;
+
+    public Node(int id){
+
+        this.id = id;
+
+        myNeighbours = new ArrayList<Node>();
+        incomingMsg = new ArrayList<String>();
+    }
+
+    public void setFail_round(int fail_round)
+    {
+        this.fail_round = fail_round;
+    }
+
+    public void setLeaderId(int incom_id)
+    {
+        this.leader_id = incom_id;
+    }
+
+    public void setElection(boolean start_election)
+    {
+        this.start_election = start_election;
+    }
+
+    public boolean isLeader()
+    {
+        return this.leader;
+    }
+
+    public void setNextNode(Node nextNode)
+    {
+        this.next_neighbour = nextNode;
+    }
+
+    public Node getNextNode()
+    {
+        return this.next_neighbour;
+    }
+
+
+    public void setParticipant(boolean condition)
+    {
+        participant = condition;
+    }
+
+    public boolean isParticipant()
+    {
+        return participant;
+    }
+
+    public void setNetwork(Network network)
+    {
+        this.network = network;
+        System.out.println("thread "+id+" has set network");
+    }
+    // Basic methods for the Node class
+
+    public int getNodeId() {
+		/*
+		Method to get the Id of a node instance
+		*/
+		return this.id;
+    }
+
+    public boolean isNodeLeader() {
+		/*
+		Method to return true if the node is currently a leader
+		*/
+		return this.leader;
+    }
+
+    public List<Node> getNeighbors() {
+		/*
+		Method to get the neighbours of the node
+		*/
+		return this.myNeighbours;
+    }
+
+    public void addNeighbour(Node n) {
+		/*
+		Method to add a neighbour to a node
+		*/
+		this.myNeighbours.add(n);
+    }
+
+    public void receiveMsg(String m) {
+		/*
+		Method that implements the reception of an incoming message by a node
+		*/
+		//这里就单纯receive，在线程启动的时候做运算找最大的，每个participant只会发一次，incoming每次保留最大，所以如果收到两次且第二次和自己incoming里的一样，认一个Leader
+        this.incomingMsg.add(m);
+        System.out.println(id + "has received message " +  m +" in round "+network.getRound());
+
+    }
+
+    public void sendMsg(String m) {
+		/*
+		Method that implements the sending of a message by a node.
+		The message must be delivered to its recepients through the network.
+		This method need only implement the logic of the network receiving an outgoing message from a node.
+		The remainder of the logic will be implemented in the network class.
+		*/
+		network.addMessage(id, m);
+        incomingMsg.clear();
+    }
+
+    public void run() {
+        while (true)
+        {
+            if(network.getRound()==fail_round)
+            {
+                network.informNodeFailure(id);
+            }
+            if(leader)
+                System.out.println("the leader is "+id +" and this is round"+ network.getRound());
+            if(incomingMsg.size() == 0 && participant && start_election)
+            {
+                System.out.println(id + "start election and send its id to "+ next_neighbour+" in round "+network.getRound());
+                this.sendMsg("ELECT "+Integer.toString(id));
+                this.start_election = false;
+            }
+            if(incomingMsg.size() > 0)
+            {
+                // judge what type of the message and preprocess it
+                for (int i=0; i<incomingMsg.size();i++)
+                {
+                    String message = incomingMsg.get(i);
+                    String[] whole_message = message.split("\\s+");
+                    if(message.startsWith("ELECT")||message.startsWith("FORWARD"))
+                    {
+                        incomingMsg.set(i, whole_message[1]);
+                    }
+                    /*if a message starts with leader, that means that this election has been done
+                    even if other sends elect or forward, it will ignore them and send the leader*/
+                    else if(message.startsWith("LEADER"))
+                    {
+                        if(Integer.parseInt(whole_message[1]) == id)
+                        {
+                            System.out.println(id+" get his own leader message "+whole_message[1] +" and stop election "+" in round "+network.getRound());
+                        }
+                        else
+                        {
+                            System.out.println(id+" set its leader "+whole_message[1] +" and forward "+" in round "+network.getRound());
+                            this.setLeaderId(Integer.parseInt(whole_message[1]));
+                            this.sendMsg("LEADER "+whole_message[1]);
+                            incomingMsg.clear();
+                        }
+                        break;
+                    }
+                }
+
+                if(!incomingMsg.isEmpty())
+                {
+                    incomingMsg.sort(Collections.reverseOrder());
+                    System.out.println(id+" has incoming message "+incomingMsg);
+
+                    //if not participant send the max of income and id, set participant
+                    if(!participant)
+                    {
+                        System.out.println(id + "not participant and sends "+incomingMsg.get(0) +" to "+ next_neighbour+" in round "+network.getRound());
+                        this.sendMsg(Integer.toString(Math.max(Integer.parseInt(incomingMsg.get(0)), id)));
+                        this.setParticipant(true);
+                    }
+                    //if participant, send m.id if m.id bigger or do nothing if m.id smaller
+                    else {
+                        if(Integer.parseInt(incomingMsg.get(0))>id)
+                        {
+                            System.out.println(id + "participant and sends "+incomingMsg.get(0) +" to "+ next_neighbour+" in round "+network.getRound());
+                            this.sendMsg(incomingMsg.get(0));
+                        }
+                        else if(Integer.parseInt(incomingMsg.get(0))==id)
+                        {
+                            this.leader = true;
+                            this.setLeaderId(id);
+                            this.sendMsg("LEADER "+id);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            incomingMsg.clear();
+            System.out.println(id + "is now waiting"+" in round "+network.getRound());
+            try {
+                // after send all the message, release the lock and wait for notify
+                synchronized(Network.class) {
+                    Network.class.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
